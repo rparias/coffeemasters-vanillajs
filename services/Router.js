17b31b191bba2e89db7539cdd9af70e271ab517d
Ronald Arias <ariasron@hotmail.com>
2024-01-09 13:02:11 -0500

    render content using the router using the dom to remove and add content

--- services/Router.js
+++ services/Router.js
@@ -15,6 +15,26 @@ const Router = {
     if (addToHistory) {
       history.pushState({ route }, "", route)
     }
+
+    // render content
+    let pageElement = null;
+    switch(route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Menu";
+        break;
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Your Order";
+        break;
+    }
+    if (pageElement) {
+      const cache = document.querySelector("main");
+      cache.innerHTML = "";
+      cache.appendChild(pageElement);
+      window.scrollX = 0;
+      window.scrollY = 0;
+    }
   }
 }
 
