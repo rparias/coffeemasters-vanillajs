1948b3a85db924d54d3682208b2923b40a971520
Ronald Arias <ariasron@hotmail.com>
2024-01-09 12:39:13 -0500

    creates a simple router using history API

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import Store from "./services/Store.js";
+import Router from "./services/Router.js";
 import { loadData } from "./services/Menu.js";
 
 // Singleton used as global store
 window.app = {};
 app.store = Store;
+app.router = Router;
 
 window.addEventListener('DOMContentLoaded', () => {
   loadData();
+  app.router.init();
   console.log('store', app.store);
 })
---
+++ services/Router.js
@@ -0,0 +1,21 @@
+const Router = {
+  init: () => {
+    document.querySelectorAll("a.navlink").forEach((a) => {
+      a.addEventListener("click", (event) => {
+        event.preventDefault();
+        const url = event.target.getAttribute("href");
+        Router.go(url);
+      })
+    });
+    // Check and load initial URL
+    Router.go(location.pathname);
+  },
+  go: (route, addToHistory = true) => {
+    console.log(`Going to ${route}`)
+    if (addToHistory) {
+      history.pushState({ route }, "", route)
+    }
+  }
+}
+
+export default Router;
